db4a8bb22a2de79057fdb10be71ce2f745d221dc
yu <fengyu225@gmail.com>
2014-10-14 14:56:50 -0700

    minor change type check decorator

--- type_check.py
+++ type_check.py
@@ -4,6 +4,7 @@ from functools import wraps
 def type_check(*type_args,**type_kwargs):
     def dec(func):
         types_args = inspect.getcallargs(type_check,*type_args,**type_kwargs)
+        print types_args
 
         @wraps(func)
         def new_func(*args,**kwargs):
@@ -12,14 +13,16 @@ def type_check(*type_args,**type_kwargs):
                 if not isinstance(val_args['args'][i],x):
                     raise Exception("Type not match: {0} is not type {1}".format(val_args['args'][i],x))
             for k in types_args['type_kwargs']:
-                if not isinstance(val_args['kwargs'][k], types_args['type_kwargs'][k]):
+                if k in val_args['kwargs'] and not isinstance(val_args['kwargs'][k], types_args['type_kwargs'][k]):
                     raise Exception("Type not match: {0} is not type {1}".format(val_args['kwargs'][k], types_args['type_kwargs'][k]))
             return func(*args,**kwargs)
         return new_func
     return dec
 
-@type_check(float,x=list)
-def f(a,b,x=[1,2],y=(3,)):
-    return sum([a,b,x[0],y[0]])
+@type_check(int,x=list)
+def f(a,x=None):
+    print a,x
+    pass
 
-print f(1,2.0,x=[3],y=(4,))
+print f(1)
+print f(2,x=3)
