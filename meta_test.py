92d67867f85e36f8a23bdaaea996db7d9902bb52
yu <fengyu225@gmail.com>
2014-10-14 17:59:47 -0700

    metaclass test

---
+++ meta_test.py
@@ -0,0 +1,26 @@
+class meta(type):
+#        def __new__(self,*args):
+#                print args
+#                super(meta,self).__new__(args)
+#        def __init__(self,*args):
+#                print "__init__"+args
+#                super(meta,self).__init__(args)
+        def __call__(self,*args):
+                raise Exception("error")
+
+class Test(object):
+    __metaclass__ = meta
+    def func(self):
+        print "func"
+    @classmethod
+    def class_func(cls):
+        print cls
+        print "class func"
+    @staticmethod
+    def static_method(*args):
+        print args
+        print "static func"
+
+#t = Test()
+Test.static_method()
+Test.class_func()
