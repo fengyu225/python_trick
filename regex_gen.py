a91114703800641f1a34869170ffc2e12b5292b4
yu <fengyu225@gmail.com>
2014-10-21 18:55:32 -0700

    regex generator: make shorter regex

--- regex_gen.py
+++ regex_gen.py
@@ -17,6 +17,20 @@ class Trie(object):
                 curr_node.children[c] = Node(c)
             curr_node = curr_node.children[c]
 
+import pdb
+def get_longest_common_suffix(l):
+    if len(l) == 0:
+        return 0
+    if len(l) == 1:
+        return len(l[0])
+    i = 1
+    while True:
+        c = l[0][-1*i]
+        for lx in l[1:]:
+            if len(lx)<i or lx[-1*i] != c:
+                return i-1
+        i+=1
+
 
 def build(root):
     # post order traversal building regex
@@ -27,7 +41,10 @@ def build(root):
         return root.char
     if len(regs) == 1:
         return "{0}{1}".format(root.char, "|".join(regs))
-    return "{0}(?:{1})".format(root.char, "|".join(regs))
+    common_suffix_len = get_longest_common_suffix(regs)
+    suffix = regs[0][-1*common_suffix_len:len(regs[0])]
+    regs = [s[0:len(s)-common_suffix_len] for s in regs]
+    return "{0}(?:{1}){2}".format(root.char, "|".join(regs), suffix)
 
 
 def generate_regex(l):
@@ -40,8 +57,28 @@ def generate_regex(l):
 if __name__ == "__main__":
     l = [
             "app1.tmol.ash1.websys.tmcs",
+            "app1.tmol.euash1.websys.tmcs",
             "app2.tmol.ash1.websys.tmcs",
             "apt1.tmol.phx1.websys.tmcs",
             "app1.tmol.phx1.websys.tmcs"
         ]
+#    l = [
+#            "apt18.tmol.ash1.websys.tmcs",
+#            "apt19.tmol.ash1.websys.tmcs",
+#            "apt20.tmol.ash1.websys.tmcs",
+#            "apt21.tmol.ash1.websys.tmcs",
+#            "apt22.tmol.ash1.websys.tmcs",
+#            "apt23.tmol.ash1.websys.tmcs",
+#            "apt24.tmol.ash1.websys.tmcs",
+#            "apt25.tmol.ash1.websys.tmcs",
+#            "apt26.tmol.ash1.websys.tmcs",
+#            "apt27.tmol.ash1.websys.tmcs",
+#            "apt28.tmol.ash1.websys.tmcs",
+#            "apt29.tmol.ash1.websys.tmcs",
+#            "apt30.tmol.ash1.websys.tmcs",
+#            "apt31.tmol.ash1.websys.tmcs",
+#            "apt32.tmol.ash1.websys.tmcs",
+#            "apt33.tmol.ash1.websys.tmcs",
+#            "apt34.tmol.ash1.websys.tmcs"
+#        ]
     print generate_regex(l)
